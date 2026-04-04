c3f83419104e0a6114da5962b5405770f41a2520
D4r3N <enzo.suares@icloud.com>
2026-04-04 22:26:52 +0200

    add response attributes for automatic doc generation

--- GameLibraryApi/Controllers/GamesController.cs
+++ GameLibraryApi/Controllers/GamesController.cs
@@ -10,75 +10,83 @@ namespace GameLibraryApi.Controllers;
 [Route("api/[controller]")]
 public class GamesController(AppDbContext db) : ControllerBase
 {
-    [HttpPost]
-    public async Task<IActionResult> Create([FromBody] CreateGameDto dto)
-    {
-        var newGame = new Game
-        {
-            Id = Guid.NewGuid().ToString(),
-            Title = dto.Title,
-            Platforms = dto.Platforms,
-            Genres = dto.Genres,
-            Status = dto.Status,
-            Rating = dto.Rating,
-        };
-        db.Games.Add(newGame);
-        await db.SaveChangesAsync();
-        return CreatedAtAction(nameof(GetById), new { id = newGame.Id }, new GameResponseDto(newGame));
-    }
+	[HttpPost]
+	[ProducesResponseType(typeof(GameResponseDto), StatusCodes.Status201Created)]
+	public async Task<IActionResult> Create([FromBody] CreateGameDto dto)
+	{
+		var newGame = new Game
+		{
+			Id = Guid.NewGuid().ToString(),
+			Title = dto.Title,
+			Platforms = dto.Platforms,
+			Genres = dto.Genres,
+			Status = dto.Status,
+			Rating = dto.Rating,
+		};
+		db.Games.Add(newGame);
+		await db.SaveChangesAsync();
+		return CreatedAtAction(nameof(GetById), new { id = newGame.Id }, new GameResponseDto(newGame));
+	}
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetById(string id)
-    {
-        var game = await db.Games.FindAsync(id);
-        return game is null ? NotFound() : Ok(new GameResponseDto(game));
-    }
+	[HttpGet("{id}")]
+	[ProducesResponseType(typeof(GameResponseDto), StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> GetById(string id)
+	{
+		var game = await db.Games.FindAsync(id);
+		return game is null ? NotFound() : Ok(new GameResponseDto(game));
+	}
 
-    [HttpGet]
-    public async Task<IActionResult> Get([FromQuery] Status? status, [FromQuery] Platform? platform, [FromQuery] string? title)
-    {
-        IQueryable<Game> query = db.Games;
+	[HttpGet]
+	[ProducesResponseType(typeof(List<GameResponseDto>), StatusCodes.Status200OK)]
+	public async Task<IActionResult> Get([FromQuery] Status? status, [FromQuery] Platform? platform, [FromQuery] string? title)
+	{
+		IQueryable<Game> query = db.Games;
 
-        if (status is not null)
-            query = query.Where(g => g.Status == status);
+		if (status is not null)
+			query = query.Where(g => g.Status == status);
 
-        if (platform is not null) 
-            query = query.Where(g => g.Platforms.Contains(platform.Value));
+		if (platform is not null)
+			query = query.Where(g => g.Platforms.Contains(platform.Value));
 
-        if (!string.IsNullOrWhiteSpace(title))
-            query = query.Where(g => g.Title.Contains(title));
+		if (!string.IsNullOrWhiteSpace(title))
+			query = query.Where(g => g.Title.Contains(title));
 
-        List<GameResponseDto> games = await query.Select(g => new GameResponseDto(g)).ToListAsync();
-        return Ok(games);
-    }
+		List<GameResponseDto> games = await query.Select(g => new GameResponseDto(g)).ToListAsync();
+		return Ok(games);
+	}
 
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Update(string id, [FromBody] UpdateGameDto dto)
-    {
-        Game? existingGame = await db.Games.FindAsync(id);
-        if (existingGame is null) return NotFound();
+	[HttpPut("{id}")]
+	[ProducesResponseType(typeof(GameResponseDto), StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> Update(string id, [FromBody] UpdateGameDto dto)
+	{
+		Game? existingGame = await db.Games.FindAsync(id);
+		if (existingGame is null) return NotFound();
 
-        existingGame.Title = dto.Title;
-        existingGame.Platforms = dto.Platforms;
-        existingGame.Genres = dto.Genres;
-        existingGame.Status = dto.Status;
-        existingGame.Rating =  dto.Rating;
+		existingGame.Title = dto.Title;
+		existingGame.Platforms = dto.Platforms;
+		existingGame.Genres = dto.Genres;
+		existingGame.Status = dto.Status;
+		existingGame.Rating = dto.Rating;
 
-        await db.SaveChangesAsync();
+		await db.SaveChangesAsync();
 
-        return Ok(new GameResponseDto(existingGame));
-    }
+		return Ok(new GameResponseDto(existingGame));
+	}
 
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(string id)
-    {
-        var game = await db.Games.FindAsync(id);
-        if (game is null) return NotFound();
+	[HttpDelete("{id}")]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> Delete(string id)
+	{
+		var game = await db.Games.FindAsync(id);
+		if (game is null) return NotFound();
 
-        db.Games.Remove(game);
-        
-        await db.SaveChangesAsync();
+		db.Games.Remove(game);
 
-        return NoContent();
-    }
+		await db.SaveChangesAsync();
+
+		return NoContent();
+	}
 }
