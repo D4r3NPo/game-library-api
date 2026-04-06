9da05a16f316ef4485c2cfcb24f02417f5cb2fca
D4r3N <enzo.suares@icloud.com>
2026-04-06 20:46:44 +0200

    map delete feature in GameClient

--- GameLibraryFrontend/Services/GameClient.cs
+++ GameLibraryFrontend/Services/GameClient.cs
@@ -11,11 +11,17 @@ public class GameClient(HttpClient http)
         return await _http.GetFromJsonAsync<List<GameResponseDto>>("api/games");
     }
 
-     public async Task<GameResponseDto?> CreateGameAsync(CreateGameDto dto)
+    public async Task<GameResponseDto?> CreateGameAsync(CreateGameDto dto)
     {
         var response = await _http.PostAsJsonAsync("api/games", dto);
         response.EnsureSuccessStatusCode();
 
         return await response.Content.ReadFromJsonAsync<GameResponseDto>();
+    }
+
+    public async Task DeleteAsync(string id)
+    {
+        var response = await _http.DeleteAsync($"api/games/{id}");
+        response.EnsureSuccessStatusCode();
     }
 }
